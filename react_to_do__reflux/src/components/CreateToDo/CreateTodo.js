91d5b5d032800c880356277354c2a512f587613e
Anil Namde <anilnamde@gmail.com>
2017-02-06 21:18:22 +0530

    added new to do projcet to undertand the refluxjs

---
+++ react_to_do__reflux/src/components/CreateToDo/CreateTodo.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import './CreateTodo';
+
+
+export default class CreateTodo extends React.Component{
+    constructor(props){
+        super(props);
+        this.props = props;
+
+        this.state = {
+            text: ''
+        };
+
+        this.onTextChange = this.onTextChange.bind(this);
+        this.onAdd = this.onAdd.bind(this);
+    }
+
+    onTextChange({target}){
+        console.log(target.value)
+        this.setState(() => {
+            return {
+                text: target.value
+            };
+        });
+    }
+
+    onAdd(){
+        console.log(this.state)
+        this.props.addItem(this.state.text);
+        this.setState(()=>{
+            return {
+                text: ''
+            }
+        });
+    }
+
+    render(){
+        return (<div>
+            <input type="text"
+                   value={this.state.text}
+                   placeholder="[Enter To Do]"
+                   onChange={this.onTextChange}/>
+
+            <input type="button"
+                   value="Add"
+                   onClick={this.onAdd}/>
+        </div>)
+    }
+}
