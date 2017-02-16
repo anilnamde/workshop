eadb72df54e31265b12965b0a00889578d9fb2bd
Anil Namde <anilnamde@gmail.com>
2017-02-16 09:42:04 +0530

    renaming the test files for checking the version of the file

---
+++ react_to_do__reflux/src/components/App/App.es5.test.js
@@ -0,0 +1,20 @@
+// import jsdom from 'mocha-jsdom';
+//
+// before(()=> {
+//     jsdom();
+//     global.React = ReactJS;
+//     console.log(this)
+// });
+//
+// const ReactDOM = require('react-dom'),
+//     React  = require('react'),
+//     App = require('./App');
+//
+// describe("something ", ()=> {
+//
+//     it('renders without crashing', () => {
+//         const div = document.createElement('div');
+//         ReactDOM.render(<App />, div);
+//     });
+//
+// });
--- react_to_do__reflux/src/components/App/App.test.js
+++ react_to_do__reflux/src/components/App/App.test.js
@@ -1,22 +1,13 @@
-import React from 'react';
+import jsdom from 'mocha-jsdom';
+
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import jsdom from 'mocha-jsdom';
+describe("something ", ()=> {
 
-describe("something ", ()=>{
-  before(()=>{
-    jsdom();
-    global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-    global.window = document.defaultView;
-    global.navigator = {userAgent: 'node.js'};
-      global.React = React;
-      global.window.React = React;
-  });
-
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-  });
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+    });
 
 });
