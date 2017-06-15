a006b2a11d8199ebeba59797124fa2a14c5b5ba2
Anil Namde <anilnamde@gmail.com>
2017-06-15 22:19:48 +0530

    chapter 2 in progress

--- websocket/ch2/server.js
+++ websocket/ch2/server.js
@@ -2,9 +2,24 @@ var WebSocketServer = require('ws').Server;
 
 var wss = new WebSocketServer({port: 8181});
 
-wss.on('connection', function(ws) { 
-    console.log('client connected'); 
-    ws.on('message', function(message) {
-        console.log(message);
-    });
+
+wss.on('connection', function (ws) {
+  console.log('client connected');
+
+  var counter = 0;
+  var interval = setInterval(function () {
+    counter = counter + 1;
+    ws.send(counter);
+  }, 1000);
+
+
+  ws.on('message', function (message) {
+    console.log(message);
+  });
+
+  ws.on('close', function () {
+    clearInterval(interval);
+    console.log('client closed');
+  });
+
 });
