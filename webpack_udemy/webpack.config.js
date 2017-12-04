bdfd74a4b07ecef5e973cdd5fdfbf8ebdca170b6
Anil Namde <anilnamde@gmail.com>
2017-12-05 01:12:10 +0530

    extracted css in separte files

--- webpack_udemy/webpack.config.js
+++ webpack_udemy/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const config = {
     entry: './src/index.js',
@@ -13,11 +14,16 @@ const config = {
                 test: /\.js$/,
             },
             {
-                use: ['style-loader', 'css-loader'],
                 test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                  use: "css-loader"
+                })
             }
         ]
-    }
+    },
+     plugins: [
+        new ExtractTextPlugin("styles.css"),
+    ]
 };
 
 module.exports = config;
